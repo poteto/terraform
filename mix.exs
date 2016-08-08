a934f1ea6ba06a9103a0a4ca685bc72d0c279c38
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2016-08-07 17:53:44 -0700

    Update meta for publishing

--- mix.exs
+++ mix.exs
@@ -3,11 +3,27 @@ defmodule Terraform.Mixfile do
 
   def project do
     [app: :terraform,
-     version: "0.1.0",
+     version: "0.0.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
+  end
+
+  def description do
+    """
+    Incrementally replace an old API with Phoenix.
+    """
+  end
+
+  def package do
+    [
+      maintainers: ["Lauren Tan"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/poteto/terraform"}
+    ]
   end
 
   # Configuration for the OTP application
