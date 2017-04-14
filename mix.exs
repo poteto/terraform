723a0d028e2be8a09ba2ab8c71db68547c3a333e
Lauren Tan <laurent@netflix.com>
2017-04-14 15:30:17 -0700

    Version bump to 0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Terraform.Mixfile do
 
   def project do
     [app: :terraform,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
