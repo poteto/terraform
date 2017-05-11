dcb0efab7545f78e7395bee9749b97054268e49e
Lauren Tan <laurent@netflix.com>
2017-05-11 14:22:07 -0700

    Released v1.0.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Terraform.Mixfile do
 
   def project do
     [app: :terraform,
-     version: "0.1.2",
+     version: "1.0.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
